b668df639fbda5ee3fcfba5b4f470c5ecd71238a
Daneal O'Habib <49035959+danealohabib@users.noreply.github.com>
2021-04-23 03:17:35 -0400

    Add files via upload

---
+++ master_output.R
@@ -0,0 +1,13 @@
+
+# load and install packages all packages used generating output
+
+# load and install packages
+if (!require("pacman")) install.packages("pacman")
+
+# output tables
+source("script/output_1.R")
+
+# output figure
+source("script/output_2.R")
+
+
